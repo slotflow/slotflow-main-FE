55a09645d917e33ef61d3a2eac33ec9142bfa9fa
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-03-18 04:05:55 +0530

    Update Provider forms design

--- src/pages/provider/ProviderAddAddress.tsx
+++ src/pages/provider/ProviderAddAddress.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/Button/CustomButton";
 import InputField from "@/components/form/InputFieldWithLable"
 import { addProviderAddress } from "@/utils/apis/provider.api";
 import { AppDispatch, RootState } from "@/utils/redux/appStore";
@@ -40,138 +41,152 @@ const ProviderAddAddress = () => {
             return;
         }
         setLoading(true);
-        console.log("formData : ",formData);
+        console.log("formData : ", formData);
         if (user?._id) {
             dispatch(addProviderAddress({ providerId: user._id, formData }))
-            .unwrap()
-            .then((res) => {
-                if(res.success){
-                    toast.success(res.message);
-                    dispatch(setAddress(true));
-                    setFormData({
-                        addressLine: "",
-                        phone: "",
-                        place: "",
-                        city: "",
-                        district: "",
-                        pincode: "",
-                        state: "",
-                        country: "",
-                        googleMapLink: "",
-                    });
-                }else{
-                    toast.error(res.message);
-                }
-            })
-            .catch((error) => {
-                toast.error(error.response.data.message);
-            })
+                .unwrap()
+                .then((res) => {
+                    if (res.success) {
+                        toast.success(res.message);
+                        dispatch(setAddress(true));
+                        setFormData({
+                            addressLine: "",
+                            phone: "",
+                            place: "",
+                            city: "",
+                            district: "",
+                            pincode: "",
+                            state: "",
+                            country: "",
+                            googleMapLink: "",
+                        });
+                    } else {
+                        toast.error(res.message);
+                    }
+                })
+                .catch((error) => {
+                    toast.error(error.response.data.message);
+                })
         } else {
             toast.error("Something went wrong, please login again and try.");
         }
     }
 
     return (
-        <div className="h-screen pt-16 flex justify-center bg-[var(--background)]">
-            <div className="p-2 w-11/12 md:w-10/12 lg:w-8/12 mt-10">
-                <h4 className="text-2xl font-semibold mb-6 text-start">Service Address</h4>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <InputField
-                            label="Address Line"
-                            id="addressLine"
-                            placeholder="Address"
-                            type="text"
-                            value={formData.addressLine}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Phone"
-                            id="phone"
-                            placeholder="+91 0000000000"
-                            type="text"
-                            value={formData.phone}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Place"
-                            id="place"
-                            placeholder="Place"
-                            type="text"
-                            value={formData.place}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="City"
-                            id="city"
-                            placeholder="City"
-                            type="text"
-                            value={formData.city}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Distrcit"
-                            id="district"
-                            placeholder="Distrcit"
-                            type="text"
-                            value={formData.district}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Pincode"
-                            id="pincode"
-                            placeholder="000000"
-                            type="text"
-                            value={formData.pincode}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="State"
-                            id="state"
-                            placeholder="State"
-                            type="text"
-                            value={formData.state}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Country"
-                            id="country"
-                            placeholder="Country"
-                            type="text"
-                            value={formData.country}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
-                        <InputField
-                            label="Google Map Link"
-                            id="googleMapLink"
-                            placeholder="https://googlemap"
-                            type="text"
-                            value={formData.googleMapLink}
-                            onChange={handleChange}
-                            required={true}
-                            onHasError={handleErrorChange}
-                        />
+
+        <div className="min-h-screen pt-16 flex justify-center w-full bg-[var(--background)]">
+            <div className="w-8/12 px-10">
+                <form onSubmit={handleSubmit} className="mt-10 p-12">
+                    <h4 className="text-2xl font-semibold mb-6 text-start">Lets fillout your Address</h4>
+                    <div className="flex w-full flex-col md:flex-row">
+                        <div className="w-full md:w-1/2 py-6 space-y-6">
+                            <InputField
+                                label="Address Line"
+                                id="addressLine"
+                                placeholder="Address"
+                                type="text"
+                                value={formData.addressLine}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Phone"
+                                id="phone"
+                                placeholder="+91 0000000000"
+                                type="text"
+                                value={formData.phone}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Place"
+                                id="place"
+                                placeholder="Place"
+                                type="text"
+                                value={formData.place}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="City"
+                                id="city"
+                                placeholder="City"
+                                type="text"
+                                value={formData.city}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Distrcit"
+                                id="district"
+                                placeholder="Distrcit"
+                                type="text"
+                                value={formData.district}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Pincode"
+                                id="pincode"
+                                placeholder="000000"
+                                type="text"
+                                value={formData.pincode}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                        </div>
+                        <div className="w-full md:w-1/2 p-6 space-y-6">
+
+                            <InputField
+                                label="State"
+                                id="state"
+                                placeholder="State"
+                                type="text"
+                                value={formData.state}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Country"
+                                id="country"
+                                placeholder="Country"
+                                type="text"
+                                value={formData.country}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                            <InputField
+                                label="Google Map Link"
+                                id="googleMapLink"
+                                placeholder="https://googlemap"
+                                type="text"
+                                value={formData.googleMapLink}
+                                onChange={handleChange}
+                                required={true}
+                                onHasError={handleErrorChange}
+                            />
+                        </div>
                     </div>
                     <div className="mt-10 flex justify-end">
-                        <button type="submit" className="w-full md:w-1/4 rounded-md bg-[var(--mainColor)] hover:bg-[var(--mainColorHover)] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--mainColor)] cursor-pointer">{loading ? "Loading" : "Next"}</button>
+                        <CustomButton props={{ loading }} />
                     </div>
                 </form>
+            </div>
+            <div className="bg-violet-200 w-4/12 p-10">
+                <h3 className="p-6 text-2xl font-bold italic">Slotflow</h3>
+                <div className="w-full p-6 space-y-2 flex justify-between">
+                    <div className="h-2 w-4/12 bg-purple-500 mx-1 rounded-md"></div>
+                    <div className="h-2 w-4/12 bg-white mx-1 rounded-md"></div>
+                    <div className="h-2 w-4/12 bg-white mx-1 rounded-md"></div>
+                </div>
             </div>
         </div>
     )
--- src/pages/provider/ProviderAddServiceDetails.tsx
+++ src/pages/provider/ProviderAddServiceDetails.tsx
@@ -7,6 +7,7 @@ import { addProviderServiceDetails } from "@/utils/apis/provider.api";
 import SelectFiledWithLabel from "@/components/form/SelectFiledWithLabel";
 import React, { ChangeEvent, FormEvent, useCallback, useEffect, useState } from "react";
 import { setServiceDetails } from "@/utils/redux/slices/authSlice";
+import CustomButton from "@/components/Button/CustomButton";
 
 interface Service {
   serviceName: string
@@ -31,7 +32,7 @@ const ProviderAddServiceDetails = () => {
   });
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-     e.preventDefault();
+    e.preventDefault();
     const file = e.target.files?.[0];
     if (file) {
       setSelectedImage(file);
@@ -82,7 +83,7 @@ const ProviderAddServiceDetails = () => {
         formDataToSend.append('providerAdhaar', formData.providerAdhaar);
         formDataToSend.append('providerExperience', formData.providerExperience);
         if (selectedImage) {
-            formDataToSend.append('certificate', selectedImage);
+          formDataToSend.append('certificate', selectedImage);
         }
         dispatch(addProviderServiceDetails({ providerId: user._id, formData: formDataToSend }))
           .unwrap()
@@ -113,102 +114,110 @@ const ProviderAddServiceDetails = () => {
     }
   };
   return (
-    <div className="h-screen pt-16 flex justify-center bg-[var(--background)]">
-      <div className="p-2 w-11/12 md:w-10/12 lg:w-8/12 mt-10">
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <h4 className="text-2xl font-semibold mb-6 text-start">Service Details</h4>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <SelectFiledWithLabel
-              label="Service Category"
-              id="serviceCategory"
-              value={formData.serviceCategory}
-              onChange={handleChange}
-              options={services}
-              required={true}
-              onHasError={handleErrorChange}
-            />
-            <InputField
-              label="Service Name"
-              id="serviceName"
-              placeholder="Service name"
-              type="text"
-              value={formData.serviceName}
-              onChange={handleChange}
-              required={true}
-              onHasError={handleErrorChange}
-            />
-            <InputField
-              label="Service Description"
-              id="serviceDescription"
-              placeholder="Service description"
-              type="text"
-              value={formData.serviceDescription}
-              onChange={handleChange}
-              required={true}
-              onHasError={handleErrorChange}
-            />
-            <InputField
-              label="Service Price"
-              id="servicePrice"
-              placeholder="₹ 1000"
-              type="number"
-              value={formData.servicePrice}
-              onChange={handleChange}
-              required={true}
-              onHasError={handleErrorChange}
-            />
-            <InputField
-              label="Adhaar Last 6 digits"
-              id="providerAdhaar"
-              placeholder="Adhaar number"
-              type="text"
-              value={formData.providerAdhaar}
-              onChange={handleChange}
-              required={true}
-              onHasError={handleErrorChange}
-            />
-            <InputField
-              label="Experience"
-              id="providerExperience"
-              placeholder="experience"
-              type="text"
-              value={formData.providerExperience}
-              onChange={handleChange}
-              required={true}
-              onHasError={handleErrorChange}
-            />
+    <div className="min-h-screen pt-16 flex justify-center w-full bg-[var(--background)]">
+      <div className="w-8/12 px-10">
+        <form onSubmit={handleSubmit} className="mt-10 p-12">
+          <h4 className="text-2xl font-semibold mb-6 text-start">Lets fillout your service details</h4>
+          <div className="flex w-full flex-col md:flex-row">
+            <div className="w-full md:w-1/2 py-6 space-y-6">
+              <SelectFiledWithLabel
+                label="Service Category"
+                id="serviceCategory"
+                value={formData.serviceCategory}
+                onChange={handleChange}
+                options={services}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+              <InputField
+                label="Service Name"
+                id="serviceName"
+                placeholder="Service name"
+                type="text"
+                value={formData.serviceName}
+                onChange={handleChange}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+              <InputField
+                label="Service Description"
+                id="serviceDescription"
+                placeholder="Service description"
+                type="text"
+                value={formData.serviceDescription}
+                onChange={handleChange}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+              <InputField
+                label="Service Price"
+                id="servicePrice"
+                placeholder="₹ 1000"
+                type="number"
+                value={formData.servicePrice}
+                onChange={handleChange}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+              <InputField
+                label="Experience"
+                id="providerExperience"
+                placeholder="experience"
+                type="text"
+                value={formData.providerExperience}
+                onChange={handleChange}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+              <InputField
+                label="Adhaar Last 6 digits"
+                id="providerAdhaar"
+                placeholder="Adhaar number"
+                type="text"
+                value={formData.providerAdhaar}
+                onChange={handleChange}
+                required={true}
+                onHasError={handleErrorChange}
+              />
+            </div>
+
+            <div className="w-full md:w-1/2 p-6 space-y-6">
             <div>
-              <label className="block text-xs md:text-sm/6 font-medium text-[var(--textTwo)] hover:text-[var(--textTwoHover)]">
-                Certificate
-              </label>
-              <div className="mt-2">
-                <input
-                  placeholder="Certificate jpg or png"
-                  name="certificate"
-                  type="file"
-                  required={true}
-                  onChange={handleImageUpload}
-                  className="block w-full rounded-md bg-[var(--inputBg)] px-2 py-2 md:px-3 md:py-2 text-[var(--textOne)] outline-1 -outline-offset-1 outline-[var(--boxBorder)] placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[var(--mainColor)] text-xs  md:text-sm"
-                />
+                <label className="block text-xs md:text-sm/6 font-medium text-[var(--textTwo)] hover:text-[var(--textTwoHover)]">
+                  Certificate
+                </label>
+                <div className="mt-2">
+                  <input
+                    placeholder="Certificate jpg or png"
+                    name="certificate"
+                    type="file"
+                    required={true}
+                    onChange={handleImageUpload}
+                    className="block w-full rounded-md bg-[var(--inputBg)] px-2 py-2 md:px-3 md:py-2 text-[var(--textOne)] outline-1 -outline-offset-1 outline-[var(--boxBorder)] placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[var(--mainColor)] text-xs  md:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-xs md:text-sm/6 font-medium text-[var(--textTwo)] hover:text-[var(--textTwoHover)]">
+                  Certificate Preview
+                </label>
+                <div className="mt-2 p-2 h-64 border-2 border-[var(--boxBorder)] rounded-lg flex justify-center items-center">
+                  <img
+                    src={previewImage || '/images/imagePlaceholder.png'}
+                    className="object-contain max-h-full max-w-full"
+                    alt="Certificate Preview"
+                  />
+                </div>
               </div>
             </div>
-            <div>
-              <label className="block text-xs md:text-sm/6 font-medium text-[var(--textTwo)] hover:text-[var(--textTwoHover)]">
-                Certificate Preview
-              </label>
-              <div className="mt-2 p-2 h-64 border-2 border-[var(--boxBorder)] rounded-lg flex justify-center items-center">
-                <img
-                  src={previewImage || '/images/imagePlaceholder.png'}
-                  className="object-contain max-h-full max-w-full"
-                  alt="Certificate Preview"
-                />
-              </div>
-            </div>
-            </div>
+          </div>
           <div className="mt-10 flex justify-end">
-            <button type="submit" className="w-full md:w-1/4 rounded-md bg-[var(--mainColor)] hover:bg-[var(--mainColorHover)] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--mainColor)] cursor-pointer">{loading ? "Loading" : "Next"}</button>
+            <CustomButton props={{ loading }} />
           </div>
         </form>
+      </div>
+      <div className="bg-violet-200 w-4/12">
+
       </div>
     </div>
   )
