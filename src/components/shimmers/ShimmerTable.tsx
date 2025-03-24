fa9cb310a3399df13b266296c959e7424fa7f220
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-03-24 17:15:55 +0530

    Shimmer table update and reduced code

--- src/components/shimmers/ShimmerTable.tsx
+++ src/components/shimmers/ShimmerTable.tsx
@@ -1,41 +1,13 @@
 const ShimmerTable = () => {
     return (
         <div className="flex flex-col rounded-md overflow-hidden border-1">
-            <div className="h-10 flex items-center border-b-1 mb-1">
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-            </div>
-            <div className="h-10 flex items-center border-b-1 mb-1">
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-            </div>
-            <div className="h-10 flex items-center border-b-1 mb-1">
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-            </div>
-            <div className="h-10 flex items-center border-b-1 mb-1">
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-            </div>
-            <div className="h-10 flex items-center mb-1">
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-                <div className="h-5 w-1/5 shimmer mx-2"></div>
-            </div>
+            {Array.from({ length: 5 }).map((_, index) => (
+                <div key={index} className="h-10 flex items-center border-b-1 mb-1">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <div key={index} className="h-5 w-1/5 shimmer mx-2"></div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 };
