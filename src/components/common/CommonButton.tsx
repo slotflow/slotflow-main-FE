e7ba205e7454d2c8dffac26e42ecbac5ab0c676f
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-04-04 09:20:41 +0530

    CommonButton padding updated

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -5,7 +5,7 @@ const CommonButton: React.FC<CommonButtonProps> = ({ onClick, text, type }) => {
   return (
     <button
       onClick={onClick}
-      className={`type-${type} py-1 px-2 md:py-1 text-xs sm:text-xs md:text-sm lg:text-[16px] border border-[var(--mainColor)] rounded-md shadow-md hover:bg-[var(--mainColor)] hover:text-white cursor-pointer`}
+      className={`type-${type} py-1 px-2 md:py-2 md:px-6 text-xs sm:text-xs md:text-sm lg:text-[16px] border border-[var(--mainColor)] rounded-md shadow-md hover:bg-[var(--mainColor)] hover:text-white cursor-pointer`}
     >
       {text}
     </button>
