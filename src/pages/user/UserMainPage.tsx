199dbf211875e88b811cbd8b0e1f54d6444b2cea
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-07-10 23:19:01 +0530

    refactor: user Dahsboard filter visibility controlled only for the dashboard

--- src/pages/user/UserMainPage.tsx
+++ src/pages/user/UserMainPage.tsx
@@ -21,13 +21,17 @@ const UserMainPage = () => {
     }
   }, [dispatch, location]);
 
+  console.log("location : ",location);
+
   return (
     <div className="flex h-screen pt-16 bg-[var(--background)] transition all duration-300">
       <Sidebar routes={userRoutes} />
       <div className={`flex-1 flex flex-col ${sidebarOpen ? 'w-[84%]' : 'w-[95%]'} transition-all duration-300`}>
         <div className="flex-1 overflow-y-auto overscroll-y-contain px-4 py-6 relative">
           <Outlet />
-          <FilterRightSideBar onClose={() => dispatch(toggleFilterSideBar())} />
+          {location?.pathname === '/user/dashboard' && (
+            <FilterRightSideBar onClose={() => dispatch(toggleFilterSideBar())} />
+          )}
         </div>
       </div>
     </div>
