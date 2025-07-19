249f965712371b49d02d8cdf53a93512b4e71e1b
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-07-20 03:29:54 +0530

    Landing page header updated and optimised

--- src/components/Navs/Header.tsx
+++ src/components/Navs/Header.tsx
@@ -1,24 +1,16 @@
+import { Link } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import { Moon, Sun } from 'lucide-react';
-import { greetings } from '@/utils/helper';
 import { navigation } from '@/utils/constants';
-import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { UserData } from '@/utils/interface/sliceInterface';
-import { handleSignoutHelper } from '@/utils/helper/signout';
 import { toggleTheme } from '@/utils/redux/slices/stateSlice';
 import { AppDispatch, RootState } from '../../utils/redux/appStore';
-import { useResetRedux } from '@/utils/hooks/systemHooks/useResetRedux';
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
 
   const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
-  const resetRedux = useResetRedux();
 
   const themeMode: boolean = useSelector((store: RootState) => store.state.lightTheme);
-  const user: UserData | null = useSelector((store: RootState) => store.auth?.authUser);
-  const greetingString: string = greetings();
 
   const changeTheme = (): void => {
     dispatch(toggleTheme());
@@ -33,68 +25,44 @@ const Header:React.FC = () => {
   }, [themeMode]);
 
   return (
-    <nav className={`w-full bg-[var(--background)] border-b-2 border-[var(--boxBorder)]} fixed`}>
-      <div className={`mx-auto px-2 sm:px-6 lg:px-8`}>
-        <div className="   flex h-16 items-center justify-between">
+    <nav className={`w-full bg-[var(--background)] fixed transition-colors duration-300 ease-in-out`}>
+      <div className={`max-w-7xl mx-auto flex h-16 items-center justify-between`}>
 
-          <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center ml-4">
-              <h4 className="text-[var(--mainColor)] font-bold italic text-xl">Slotflow</h4>
-            </div>
-          </div>
+        <div className='w-3/12'>
+          <h4 className="text-[var(--mainColor)] text-3xl font-bold italic hover:bg-[var(--mainColor)] hover:text-white px-2 rounded-lg cursor-pointer">Slotflow</h4>
+        </div>
 
-          {!user?.isLoggedIn ? (
-            <div className="flex flex-1 items-center justify-center">
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      aria-current={item.current ? 'page' : undefined}
-                      className="rounded-md px-3 py-2 text-sm font-medium text-[var(--textOne)] hover:text-[var(--textOneHover)]"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className="flex flex-1 items-center justify-center">
-              <div className="hidden sm:ml-6 sm:block">
-                <h2 className="text-lg font-semibold text-[var(--textOne)]">
-                  {greetingString} {user?.username ? user.username : 'User'}
-                </h2>
-              </div>
-            </div>
-          )}
-
-
-          {(user?.isLoggedIn && user?.role) && (
-            <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-[var(--textOne)] hover:bg-[var(--menuItemHoverBg)] cursor-pointer" onClick={() => {
-              handleSignoutHelper({ role: user.role, dispatch, resetRedux, navigate })
-            }}>
-              Log out
-            </button>
-          )}
-
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <div  className="relative ml-3">
-              <div className='flex'>
-                {themeMode ?
-                  <div className="relative flex rounded-full cursor-pointer mx-3" onClick={changeTheme}>
-                    <Sun />
-                  </div>
-                  :
-                  <div className="relative flex rounded-full cursor-pointer mx-3" onClick={changeTheme}>
-                    <Moon />
-                  </div>
-                }
+        <div className='w-6/12'>
+          <div className="flex flex-1 items-center justify-center">
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className="rounded-md px-3 py-2 text-sm font-medium text-[var(--textOne)] hover:text-[var(--textOneHover)]"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
         </div>
+
+        <div className='w-3/12 flex justify-end'>
+          {themeMode ?
+            <div className="relative flex rounded-full cursor-pointer mx-3" onClick={changeTheme}>
+              <Sun />
+            </div>
+            :
+            <div className="relative flex rounded-full cursor-pointer mx-3" onClick={changeTheme}>
+              <Moon />
+            </div>
+          }
+        </div>
+
       </div>
 
       {/* <div className="sm:hidden">
@@ -111,6 +79,7 @@ const Header:React.FC = () => {
           ))}
         </div>
       </div> */}
+      
     </nav>
   )
 }
