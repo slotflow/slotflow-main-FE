010585c724331006f6e9cfa2c6613ec4c0e3a6d5
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-07-20 18:36:38 +0530

    refactor : Footer updated for the responsiveness

--- src/components/Navs/FooterBar.tsx
+++ src/components/Navs/FooterBar.tsx
@@ -17,12 +17,12 @@ const FooterBar = ({
 
   return (
     <footer className={cn("bg-background w-full", className)}>
-      <div className="max-w-7xl mx-auto">
+      <div className="px-4 lg:px-0 max-w-7xl mx-auto">
         <Footer>
           <FooterContent>
             <FooterColumn className="col-span-2 sm:col-span-3 md:col-span-1">
               <div className="flex items-center gap-2">
-                <h4 className="text-[var(--mainColor)] text-2xl font-bold italic hover:bg-[var(--mainColor)] hover:text-white px-2 rounded-lg cursor-pointer">Slotflow</h4>
+                <h4 className="text-[var(--mainColor)] text-2xl font-bold italic hover:bg-[var(--mainColor)] hover:text-white rounded-lg cursor-pointer">Slotflow</h4>
               </div>
             </FooterColumn>
             {columns.map((column, index) => (
@@ -41,7 +41,7 @@ const FooterBar = ({
             ))}
             <FooterColumn className="col-span-2 sm:col-span-3 md:col-span-1">
               <div className="flex items-center gap-2">
-                <p className="text-[var(--textOne)] text-sm pt-1 px-2 text-end">{about}</p>
+                <p className="text-[var(--textOne)] text-sm pt-1 px-2 text-center md:text-end">{about}</p>
               </div>
             </FooterColumn>
           </FooterContent>
